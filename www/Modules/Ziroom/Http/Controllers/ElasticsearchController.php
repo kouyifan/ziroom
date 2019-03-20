6bb1b4605149e4338f4a15d42de5199fcf6d8673
kouyifan <kouyifan.hf@gmail.com>
2019-03-20 16:41:32 +0800

    111

--- www/Modules/Ziroom/Http/Controllers/ElasticsearchController.php
+++ www/Modules/Ziroom/Http/Controllers/ElasticsearchController.php
@@ -52,7 +52,6 @@ class ElasticsearchController extends Controller
                             ]
                         ]
                     ],
-                    'articles' => []
                 ]
             ]
         ];
