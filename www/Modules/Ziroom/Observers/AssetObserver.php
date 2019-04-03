5d86fbfe2309927174d9a754f11d55ce0602c4cf
kouyifan <kouyifan.hf@gmail.com>
2019-04-04 01:50:41 +0800

    123

--- www/Modules/Ziroom/Observers/AssetObserver.php
+++ www/Modules/Ziroom/Observers/AssetObserver.php
@@ -21,15 +21,14 @@ class AssetObserver
     public function created(Asset $asset)
     {
         $fileService = new FileSystemService();
+        $file_hash = $fileService->_get_file_hash($asset->file_path,$asset->disk);
 
-        $find = \Modules\Ziroom\Entities\Asset::where(
-            'file_hash',$fileService->_get_file_hash($asset->file_path,$asset->disk)
-        )->value('id');
+        $find = \Modules\Ziroom\Entities\Asset::where('file_hash',$file_hash)->value('id');
 
         if ($find) return false;
 
         $asset->file_size = $fileService->_get_file_size($asset->file_path,$asset->disk);
-        $asset->file_hash = $fileService->_get_file_hash($asset->file_path,$asset->disk);
+        $asset->file_hash = $file_hash;
         $asset->file_suffix = fn_get_file_ext($asset->file_path);
         $asset->file_type = strtoupper($fileService->_get_file_mime($asset->file_path,$asset->disk));
 
