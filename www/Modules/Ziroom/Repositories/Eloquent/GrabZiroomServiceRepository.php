8f4cf23ac92bdf106b4a32dd86582298210d26c3
kouyifan <kouyifan.hf@gmail.com>
2019-04-12 10:48:16 +0800

    test

--- www/Modules/Ziroom/Repositories/Eloquent/GrabZiroomServiceRepository.php
+++ www/Modules/Ziroom/Repositories/Eloquent/GrabZiroomServiceRepository.php
@@ -27,7 +27,7 @@ class GrabZiroomServiceRepository implements GrabZiroomInterface{
         $rand_ip = Rand_IP();
         $this->header = [
             'timeout' => '30',
-            'proxy'=>'http://150.109.195.167:1080',
+            'proxy'=>'https://210.150.121.110:3128',
             'headers' => [
                 "Host"  =>  "www.ziroom.com",
                 'Referer' => 'http://www.ziroom.com/',
